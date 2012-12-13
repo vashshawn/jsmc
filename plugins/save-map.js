cb085fee14b9ff2f39b9428fb6b4036b7cbd0cb7
SomeoneWeird <adam@volatiletechnology.com>
2012-12-14 00:13:03 +1100

    Move save function to map.js

--- plugins/save-map.js
+++ plugins/save-map.js
@@ -1,23 +1,7 @@
-var path = require("path"),
-    fs = require("fs");
-
 module.exports = function(location) {
   return function(game) {
     var save_interval = setInterval(function() {
-      Object.keys(game.map.chunks).forEach(function(k) {
-        if (!game.map.chunks[k].dirty) { return; }
-
-        console.log("Saving chunk " + k);
-
-        fs.writeFile(path.join(location, Buffer(k).toString("base64")), game.map.chunks[k].data, function(err) {
-          if (err) {
-            console.warn(err);
-            return;
-          }
-
-          game.map.chunks[k].dirty = false;
-        });
-      });
+      game.map.save_blocks(location);
     }, 10000);
   };
 };
--- plugins/server-stop.js
+++ plugins/server-stop.js
@@ -1,6 +1,5 @@
 var fs = require('fs'),
-    path = require('path'),
-    async = require('async');
+    path = require('path');
 
 module.exports = function() {
   return function(game) {
@@ -13,27 +12,7 @@ module.exports = function() {
         });
       });
 
-      async.forEach(Object.keys(game.map.chunks), function(k, cb) {
-        if (!game.map.chunks[k].dirty) { 
-          cb(); 
-          return; 
-        }
-
-        console.log("Saving chunk " + k);
-
-        fs.writeFile(path.normalize(__dirname + "/..") + "/map/" + Buffer(k).toString("base64"), game.map.chunks[k].data, function(err) {
-          if (err) {
-            console.warn(err);
-            return;
-          }
-          game.map.chunks[k].dirty = false;
-          cb();
-
-        });
-      }, function(err) {
-        if(err) {
-          console.warn(err);
-        }
+      game.map.save_blocks(path.normalize(__dirname + "/..") + "/map/", function() {
         console.log("\nServer is shutting down....");
         process.exit();
       });
