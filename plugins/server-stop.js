556c98e6c48a74fa599f89ddc8894e0ed39526ac
SomeoneWeird <adam@volatiletechnology.com>
2012-12-13 18:08:41 +1100

    Save blocks on server-stop

--- plugins/server-stop.js
+++ plugins/server-stop.js
@@ -1,8 +1,10 @@
+var fs = require('fs'),
+    path = require('path'),
+    async = require('async');
+
 module.exports = function() {
   return function(game) {
     process.on('SIGINT', function () {
-
-      console.log("\nServer is shutting down....");
       
       game.clients.forEach(function(client) {
         client.emit("data", {
@@ -11,7 +13,30 @@ module.exports = function() {
         });
       });
 
-      process.exit();
+      async.forEach(Object.keys(game.map.chunks), function(k, cb) {
+        if (!game.map.chunks[k].dirty) { 
+          cb(); 
+          return; 
+        }
+
+        console.log("Saving chunk " + k);
+
+        fs.writeFile(path.normalize(__dirname + "/..") + "/map/" + Buffer(k).toString("base64"), game.map.chunks[k].data, function(err) {
+          if (err) {
+            console.warn(err);
+            return;
+          }
+          game.map.chunks[k].dirty = false;
+          cb();
+
+        });
+      }, function(err) {
+        if(err) {
+          console.warn(err);
+        }
+        console.log("\nServer is shutting down....");
+        process.exit();
+      });
     });
   };
 };
