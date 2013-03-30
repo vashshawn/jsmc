4d2ec01f9cf4ee89a45f2d0bcd2c344ced5d8e70
whiskers75 <crazycats@yopmail.com>
2013-03-30 04:41:57 -0400

    Change the way we load chunks to accomodate for the new protection system

--- plugins/mapgen-load.js
+++ plugins/mapgen-load.js
@@ -8,20 +8,29 @@ var MapgenLoad = module.exports = function MapgenLoad(location) {
 MapgenLoad.prototype.modify = function modify(chunk, cb) {
   var k = [chunk.x.toString(36), chunk.z.toString(36)].join(":"),
       file = path.join(this.location, Buffer(k).toString("base64"));
-
+  var file2 = path.join(this.location, Buffer(k).toString("base64"), '.json');
   fs.exists(file, function(exists) {
-    if (!exists) {
-      return cb(null, chunk);
-    }
-
-    fs.readFile(file, function(err, data) {
-      if (err) {
-        return cb(err);
+      if (!exists) {
+          return cb(null, chunk);
       }
+      fs.exists(file2, function(exists) {
+          if (!exists) {
+              return cb(null, chunk);
+          }
+          fs.readFile(file, function(err, data) {
+              if (err) {
+                  return cb(err);
+              }
+              fs.readFile(file2, function(err, newChunk) {
+                  if (err) {
+                      return cb(err);
+                  }
+              chunk = JSON.parse(newChunk); 
+              data.copy(chunk.data);
 
-      data.copy(chunk.data);
-
-      return cb(true, chunk);
-    });
+              return cb(true, chunk);
+              });
+          });
+      });
   });
-};
+  };
