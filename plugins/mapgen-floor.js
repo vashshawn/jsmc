1af263e3ff7f19e29bf560684487d65d303fa2e6
whiskers75 <whiskers75@me.com>
2013-04-03 11:16:09 +0100

    Added chunk borders into mapgen-floor

--- plugins/mapgen-floor.js
+++ plugins/mapgen-floor.js
@@ -2,16 +2,29 @@ var MapgenFloor = module.exports = function MapgenFloor(seed) {
 };
 
 MapgenFloor.prototype.modify = function modify(chunk, cb) {
-  for (var x = 0; x < 16; ++x) {
-    for (var z = 0; z < 16; ++z) {
-      chunk.set_block_type(x, z, 0, 7);
+    for (var x = 0; x < 16; ++x) {
+	for (var z = 0; z < 16; ++z) {
+	    chunk.set_block_type(x, z, 0, 7);
+	}
     }
-  }
-  for (var x = 0; x < 16; ++x) {
-    for (var z = 0; z < 16; ++z) {
-      chunk.set_block_type(x, z, 1, 2);
+    for (var x = 0; x < 16; ++x) {
+	for (var z = 0; z < 16; ++z) {
+	    chunk.set_block_type(x, z, 1, 2);
+	}
     }
-  }
-
-  return cb(null, chunk);
+    for (var x = 0; x < 16; x++) {
+	chunk.set_block_type(x, 0, 1, 49);
+    }
+    
+    for (var z = 0; z < 16; ++z) {
+        chunk.set_block_type(0, z, 1, 49);
+    }
+    for (var x = 0; x < 16; x++) {
+        chunk.set_block_type(x, 16, 1, 49);
+    }
+    
+    for (var z = 0; z < 16; ++z) {
+        chunk.set_block_type(16, z, 1, 49);
+    }
+    return cb(null, chunk);
 };
